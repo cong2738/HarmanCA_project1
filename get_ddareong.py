d13b99fcd222b91470813dda6da5294618d98268
pumpk <cong2738@gmail.com>
2025-02-21 10:08:20 +0900

    set API param

--- get_ddareong.py
+++ get_ddareong.py
@@ -5,10 +5,9 @@ key = getenv("SEOUL_API_KEY")
 url = 'http://apis.data.go.kr/B552584/UlfptcaAlarmInqireSvc/getUlfptcaAlarmInfo'
 params ={'KEY' : key, 
          'TYPE' : 'json', 
-         'SERVICE' : '100', 
-         'START_INDEX' : '2020', 
-         'END_INDEX' : 'PM10',
-         'stationId' : 'STRING(선택)'
+         'SERVICE' : 'bikeList', 
+         'START_INDEX' : '0', 
+         'END_INDEX' : '100',
          }
 response = requests.get(url, params=params)
 print(response.content)
