05213e5328066975ae91c6150d6e9ed7932aa9f1
pumpk <cong2738@gmail.com>
2025-02-21 16:40:19 +0900

    Geocoder API 2.0을 활용한 주소-좌표 변환기

---
+++ Geocoder.py
@@ -0,0 +1,30 @@
+"""
+modDate: 2025-02-21
+Geocoder API 2.0을 활용한 주소-좌표 변환기
+"""
+
+import requests
+import os
+
+def geocoder_XYat(rode_id,road_type): #'ROAD':도로명 주소 'PARCEL':지번 주소
+    key = os.getenv("V_WORLD_KEY") 
+    import requests
+    apiurl = "https://api.vworld.kr/req/address?"
+    params = {
+        "service": "address",
+        "request": "getcoord",
+        "crs": "epsg:4326",
+        "address": rode_id,
+        "format": "json",
+        "type": road_type,
+        "key": key
+    }
+    response = requests.get(apiurl, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        x,y = tuple(data["response"]["result"]["point"].values())
+        
+    return x,y
+
+if __name__ == "__main__":
+    print(geocoder_XYat("판교로 242","ROAD") )
