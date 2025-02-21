c288e8414b81109e334b6e261f3db6f20d799f31
pumpk <cong2738@gmail.com>
2025-02-21 09:41:56 +0900

    ref

--- get_airkorea_data.py
+++ get_airkorea_data.py
@@ -1,5 +1,7 @@
 import requests
-key = _
+from os import getenv
+
+key = getenv("AKO_API_KEY")
 url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMinuDustWeekFrcstDspth'
 params ={'serviceKey' : key, 'returnType' : 'xml', 'numOfRows' : '100', 'pageNo' : '1', 'searchDate' : '2020-11-09' }
 response = requests.get(url, params=params)
