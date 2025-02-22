b0a309af94efbbb6951da4886aefee8793f9a9bf
pumpk <cong2738@gmail.com>
2025-02-23 00:21:51 +0900

    refactor: 클래스객체로 변경

--- tmap/getLoc/Geocoder.py
+++ tmap/getLoc/Geocoder.py
@@ -14,30 +14,37 @@ Geocoder API 2.0을 활용한 주소-좌표 변환기
 
 import requests
 import os
-
-def XY_at(rode_id,road_type): #'ROAD':도로명 주소 'PARCEL':지번 주소
-    key = os.getenv("V_WORLD_KEY") 
-    apiurl = "https://api.vworld.kr/req/address?"
-    params = {
-        "service": "address",
-        "request": "getcoord",
-        "crs": "epsg:4326",
-        "address": rode_id,
-        "format": "json",
-        "type": road_type,
-        "key": key
-    }
-    response = requests.get(apiurl, params=params)
-    if response.status_code != 200: # 응답완료가 아닐시 에러
-        return None
-    
-    data = response.json()["response"]
-    if data["status"] != "OK": #비정상 상태시 에러
-        return None
-    
-    (x,y) = tuple(data["result"]["point"].values())
+class Geocoder:
+    def __init__(self, rode_id, rode_type):
+        self.xy = self.setloc_at(rode_id,rode_type)
+                                
+    def setloc_at(self,rode_id,road_type): #'ROAD':도로명 주소 'PARCEL':지번 주소
+        key = os.getenv("V_WORLD_KEY") 
+        apiurl = "https://api.vworld.kr/req/address?"
+        params = {
+            "service": "address",
+            "request": "getcoord",
+            "crs": "epsg:4326",
+            "address": rode_id,
+            "format": "json",
+            "type": road_type,
+            "key": key
+        }
+        response = requests.get(apiurl, params=params)
+        if response.status_code != 200: # 응답완료가 아닐시 에러
+            return None
         
-    return x,y
+        data = response.json()["response"]
+        if data["status"] != "OK": #비정상 상태시 에러
+            return None
+        
+        (x,y) = tuple(data["result"]["point"].values())
+            
+        return x,y
+    
+    def get_loc(self):
+        return self.xy
 
 if __name__ == "__main__":
-    print(XY_at("판교로 242","ROAD") )
+    loc = Geocoder("판교로 242","ROAD")
+    print(loc.get_loc() )
