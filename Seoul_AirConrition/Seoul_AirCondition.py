7751c48ca8d6533b186e027c22750c612f05fcd1
pumpk <cong2738@gmail.com>
2025-02-23 01:15:31 +0900

    의미없는 주석 삭제

--- Seoul_AirConrition/Seoul_AirCondition.py
+++ Seoul_AirConrition/Seoul_AirCondition.py
@@ -12,7 +12,7 @@ class Seoul_Air_Quality:
         self.Seoul_Air_Quality_dict = self.set_seoul_air_quality()
 
     def set_seoul_air_quality(self):
-        api_key = os.getenv("JIHO_SEOUL_API_KEY") # 여기에 서울시 API 키 입력
+        api_key = os.getenv("JIHO_SEOUL_API_KEY") 
         res = dict()
         url = f"http://openAPI.seoul.go.kr:8088/{api_key}/json/RealtimeCityAir/1/100/"
         response = requests.get(url)
