eb681c41e259771999fd8897604c6729bfd70053
pumpk <cong2738@gmail.com>
2025-02-22 16:14:19 +0900

    refactoring: depth줄이기

--- get_location/tmap_load_trip.py
+++ get_location/tmap_load_trip.py
@@ -17,42 +17,47 @@ def get_route (start_adress, end_adress, adressType):
     print(start_stationXY.get_stationLoc())
     print(end_stationXY.get_stationLoc())
 
-    if start_stationXY.get_stationLoc() and end_stationXY.get_stationLoc():
-        sx,sy = start_stationXY.get_stationLoc()
-        ex,ey = end_stationXY.get_stationLoc()
+    # 두위치중 하나라도 None이라면 에러
+    if not (start_stationXY.get_stationLoc() and end_stationXY.get_stationLoc()): 
+        return None
+    
+    sx,sy = start_stationXY.get_stationLoc()
+    ex,ey = end_stationXY.get_stationLoc()
 
-        # 요청 헤더
-        headers = {
-            "accept": "application/json",
-            "appKey": TEST_KEY,  # 실제 API 키 사용
-            "content-type": "application/json"
-        }
+    # 요청 헤더
+    headers = {
+        "accept": "application/json",
+        "appKey": TEST_KEY,  # 실제 API 키 사용
+        "content-type": "application/json"
+    }
 
-        # 요청 데이터 (JSON)
-        data = {
-            "startX": sx,
-            "startY": sy,
-            "endX": ex,
-            "endY": ey,
-            "lang": 0,
-            "format": "json",
-            "count": 10,
-        }   
+    # 요청 데이터 (JSON)
+    data = {
+        "startX": sx,
+        "startY": sy,
+        "endX": ex,
+        "endY": ey,
+        "lang": 0,
+        "format": "json",
+        "count": 10,
+    }   
 
-        # POST 요청 보내기
-        response = requests.post(URL, json=data, headers=headers)
+    # POST 요청 보내기
+    response = requests.post(URL, json=data, headers=headers)
+    
+    # 비정상 응답시 에러
+    if response.status_code != 200: 
+        print(f"❌ API 호출 실패: {response.status_code}, {response.text}")
+        return None
 
-        # 응답 저장
-        if response.status_code == 200:
-            response_data = response.json()
-            
-            # JSON 데이터를 파일로 저장
-            with open("tmap_subway.json", "w", encoding="utf-8") as f:
-                json.dump(response_data, f, ensure_ascii=False, indent=4)
-            
-            print("✅ tmap_subway.json 파일 저장 완료!")
-        else:
-            print(f"❌ API 호출 실패: {response.status_code}, {response.text}")
-
+    # 응답 저장
+    response_data = response.json()
+    
+    # JSON 데이터를 파일로 저장
+    with open("tmap_subway.json", "w", encoding="utf-8") as f:
+        json.dump(response_data, f, ensure_ascii=False, indent=4)
+    
+    print("✅ tmap_subway.json 파일 저장 완료!")
+    
 if __name__ == "__main__":
     print(get_route("개화동 664","목동 926-3", "ROAD"))
