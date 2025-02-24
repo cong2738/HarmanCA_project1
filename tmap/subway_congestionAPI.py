26b57caaf70a907a858c5ecd5220b1f77938dd4c
pumpk <cong2738@gmail.com>
2025-02-24 14:52:08 +0900

    혼잡도 뭉탱이로 나갈 수 있게 수정중(ongoing)

--- tmap/subway_congestionAPI.py
+++ tmap/subway_congestionAPI.py
@@ -1,9 +1,10 @@
 import requests,json,os
 
 class subway_congestionAPI:
-    def __init__(self):
-        station_code = self.set_subway_stations_code()
-        station_congestionDict = self.set_station_congestion()
+    def __init__(self,stations):
+        station_codes = self.set_subway_stations_code()
+        station_congestionDict = self.set_congestionDict(stations)
+        print(station_congestionDict)
     
     def make_stationCode_json(self):
         # 요청 헤더
@@ -27,10 +28,10 @@ class subway_congestionAPI:
             json.dump(data, file, ensure_ascii=False, indent=4)  # JSON 저장
         print("JSON 데이터가 subway_stations_code.json 파일로 저장되었습니다.")
 
-    def make_stationCongestion_json(self):
+    def make_stationCongestion_json(self,station):
         # 요청 헤더
         API_KEY = os.getenv("TMAP_PT_KEY")
-        url = "https://apis.openapi.sk.com/puzzle/subway/congestion/stat/train/stations/0"
+        url = f"https://apis.openapi.sk.com/puzzle/subway/congestion/stat/train/stations/{self.station_code[station]}"
         headers = {
             "appkey": API_KEY
         }
@@ -46,25 +47,24 @@ class subway_congestionAPI:
             json.dump(data, file, ensure_ascii=False, indent=4)  # JSON 저장
         print("JSON 데이터가 stations_congestion.json 파일로 저장되었습니다.")
 
-    def set_subway_stations_code(self):
+    def set_subway_stations_code(self) -> dict:
         try:
             stations_id = self.set_code()
         except:
             print("Fail to  open json")
-            self.make_stationCode_json()
+            # self.make_stationCode_json()
             stations_id = self.set_code()
         return stations_id
     
-    def set_station_congestion(self):
-        try:
-            station_congestion = self.set_congestion()
-        except:
-            print("Fail to  open json")
-            self.make_stationCongestion_json()
-            station_congestion = self.set_congestion()
-        return station_congestion
+    def set_congestionDict(self, stations):
+        congestionDict = dict()
+        for station in stations:
+            if station not in self.station_codes.key:
+                congestionDict[station] = self.set_congestion(station)
+        return congestionDict
+        
 
-    def set_code(self):
+    def set_code(self,):
         res = dict()
         with open("./_data/subway_stations_code.json", "r", encoding="UTF-8") as file:
             data = json.load(file)
@@ -73,11 +73,12 @@ class subway_congestionAPI:
 
         return res
     
-    def set_congestion(self):
+    def set_congestion(self,station):
+        self.make_stationCongestion_json(station)
         res = dict()
         with open("./_data/stations_congestion.json", "r", encoding="UTF-8") as file:
             data = json.load(file)
-
+        res = data["contents"]["stat"][0]["data"][0]["congestionTrain"]
         return res
 
 if __name__ == "__main__":
